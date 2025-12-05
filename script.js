943a468606f87f97783fa83878bcc9b8cc6a8134
joroha <62990050+jorohaa@users.noreply.github.com>
2025-12-05 18:31:26 +0100

    Remove helper comments

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// create two score variables humanScore and computerScore
 let humanScore = 0;
 let computerScore = 0;
 
-// call playGame()
 playGame();
 
-// create function getComputerChoice
-// create variable choice
-// get random number between 0 and 100 and put it in choice
-// if number between 0 and 33.33, getComputerChoice returns "rock"
-// if number between 33.34 and 66.66, getComputerChoice returns "paper"
-// if number between 66.67 and 100, getComputerChoice returns "scissors"
 function getComputerChoice() {
     let choice = Math.random() * 100;
 
@@ -25,24 +17,12 @@ function getComputerChoice() {
     }
 }
 
-// create function getHumanChoice
-// create variable choice
-// store input from prompt in choice
-// convert choice to lowercase
-// return choice
 function getHumanChoice() {
     let choice = prompt("Rock, paper, or scissors?");
     choice = String(choice).toLowerCase();
     return choice;
 }
 
-// create function playRound
-// give playRound parameters: humanChoice, computerChoice
-// evaluate humanChoice first, then computerChoice
-// increment score variable according to winner
-// in case humanChoice wins, return "You win! [humanChoice] beats [computerChoice]"
-// in case computerChoice wins, return "You lose! [computerChoice] beats [humanChoice]"
-// in case it's a tie, return "It's a tie! Both chose [humanChoice]"
 function playRound(humanChoice, computerChoice) {
     let humanWin = `You win! ${humanChoice} beats ${computerChoice}.`;
     let computerWin = `You lose! ${computerChoice} beats ${humanChoice}.`;
@@ -85,12 +65,6 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-// create function playGame
-// playGame loops playRound 5 times
-// --create variables humanChoice and computerChoice
-// --humanChoice and computerChoice need to be reassigned with each game
-// playGame prints scores at end of each game
-// playGame prints winner at the end
 function playGame() {
     let humanChoice;
     let computerChoice;
